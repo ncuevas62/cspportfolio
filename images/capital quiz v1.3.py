8b8d6821e6e484642ac346989b8c68bca8f0cbd3
ncuevas62 <30420951+ncuevas62@users.noreply.github.com>
2017-08-01 20:06:31 -0700

    Add files via upload

---
+++ images/capital quiz v1.3.py
@@ -0,0 +1,41 @@
+#Nancy Cuevas
+#Capital City Quiz
+
+print("What is the capital of California?\n a. Burbank\n b. Sacramento\n c. Los Angeles\n d. San Francisco")
+answer1=raw_input("Your Choice  ")
+if answer1 == "b": 
+    print "Correct!"
+else:
+    print "Sorry. The correct answer is b."
+
+print
+print("What is the capital of Arizona?\n a. Las Vegas\n b. Littlerock\n c. Sacramento\n d. Phoenix")
+answer2=raw_input("Your Choice  ")
+if answer2 == "d": 
+    print "Correct!"
+else:
+    print "Sorry. The correct answer is d."    
+
+print
+print("What is the capital of Nevada?\n a. Las Vegas\n b. Henderson\n c. Carson City\n d. Phoenix")
+answer3=raw_input("Your Choice  ")
+if answer3 == "c": 
+    print "Correct!"
+else:
+    print "Sorry. The correct answer is c."    
+
+print
+print("What is the capital of Connecticut?\n a. Hartford\n b. Dover\n c. Boise\n d. Denver")
+answer4=raw_input("Your Choice  ")
+if answer4 == "a": 
+    print "Correct!"
+else:
+    print "Sorry. The correct answer is a."       
+
+print
+print("What is the capital of Florida?\n a. Jacksonville\n b. Tampa\n c. Orlando\n d. Tallahassee")
+answer5=raw_input("Your Choice  ")
+if answer5 == "d": 
+    print "Correct!"
+else:
+    print "Sorry. The correct answer is d."       
